eb677ac8566e63266f97fb9c69b98eaac8cf409a
Darren Sarra <adriansarra18@gmail.com>
2025-09-25 19:49:17 -0400

    Add new function

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,19 +1,26 @@
 from django.shortcuts import render
-import markdown
+from markdown import markdown
 from . import util
 
-
+# index function – homepage of the wiki
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+
+# entry function – displays the contents of the encyclopedia entry
 def entry(request, title):
 
-    #convert markdown file to html file
-
+    html = markdown(util.get_entry(title))
+    print(html)
 
     return render(request, "encyclopedia/entry.html", {
         "title": title,
-        "entry": util.get_entry(title)
+        "entry": html
     })
+
+
+# new function – creates a new encyclopedia entry
+def new(request):
+    return render(request, "encyclopedia/new.html")
