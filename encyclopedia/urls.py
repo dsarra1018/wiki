03047b5f62e4bcd8a61c7130ef81626eb13d67b6
Darren Sarra <adriansarra18@gmail.com>
2025-09-27 22:26:50 -0400

    Update edit urlpatterns

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.entry, name="entry"),
+    path("wiki/<str:title>/edit", views.edit, name="edit"),
     path("new", views.new, name="new"),
     path("random", views.random_page, name="random")
 ]
