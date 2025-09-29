d406e8c6998d9edb9a93ee59f1f28babcec7fc9c
Darren Sarra <adriansarra18@gmail.com>
2025-09-28 20:05:31 -0400

    Add route for search

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("wiki/<str:title>", views.entry, name="entry"),
     path("wiki/<str:title>/edit", views.edit, name="edit"),
     path("new", views.new, name="new"),
-    path("random", views.random_page, name="random")
+    path("random", views.random_page, name="random"),
+    path("search", views.search, name="search")
 ]
